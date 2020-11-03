784956da6c6c77f5010bd669e2a3c827bd877dad
AlyzaG <alyzagumma@outlook.fr>
2020-11-03 17:48:56 +0000

    commit js

---
+++ app.js
@@ -0,0 +1,44 @@
+let firstName = "Alyza";
+let favouriteColor = "blue";
+let favouriteFood = "raclette";
+let city = "Dublin";
+
+/*
+let x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+*/
+
+/*
+var x = 'Laurel';
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+console.log(y);
+*/
+
+// shows the value that i entered in the variable previously with spaces using blank strings
+//console.log(firstName + " " + favouriteColor + " " + favouriteFood + " " + city);
+
+let phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+let endingPhrase =  "is what we see. Rainbow colours, are made with light bending you see!";
+let one = "Red";
+let two = "orange";
+let three = "yellow";
+let four = "green";
+let five = "blue";
+let six = "indigo";
+let seven = "violet";
+
+//console.log( phrase + " " + one + " " + two + " " + three + " " + four + " " + five + " " + six + " " + seven + " " + endingPhrase);
+
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+console.log(myAlphabet.length);
+
+let x = 5, y = 10, z = 15;
+
+console.log(x--);
+
+
+
